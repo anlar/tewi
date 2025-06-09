1b10c2cfa9c696da4c5e30019bf11edffc6ba8bf
Anton Larionov <11796525+anlar@users.noreply.github.com>
2025-06-09 10:25:38 +0200

    build: prepare v0.8.0

--- src/tewi/app.py
+++ src/tewi/app.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 
 from datetime import datetime
 from functools import cache, wraps
