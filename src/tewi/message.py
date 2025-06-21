49f15e3689f23d48d372bdb9a36e3a226be293f8
Anton Larionov <11796525+anlar@users.noreply.github.com>
2025-06-21 19:04:33 +0200

    ref: move torrent list to separate module

--- src/tewi/message.py
+++ src/tewi/message.py
@@ -1,4 +1,5 @@
 from textual.message import Message
+from .common import PageState
 
 
 class AddTorrent(Message):
@@ -30,3 +31,51 @@ class SortOrderSelected(Message):
         super().__init__()
         self.order = order
         self.is_asc = is_asc
+
+
+class Notification(Message):
+
+    def __init__(self,
+                 message: str,
+                 severity: str = 'information'):
+
+        super().__init__()
+        self.message = message
+        self.severity = severity
+
+
+class Confirm(Message):
+    def __init__(self, message, description, check_quit):
+        super().__init__()
+        self.message = message
+        self.description = description
+        self.check_quit = check_quit
+
+
+class OpenAddTorrent(Message):
+    pass
+
+
+class OpenUpdateTorrentLabels(Message):
+    def __init__(self, torrent, torrent_ids):
+        super().__init__()
+        self.torrent = torrent
+        self.torrent_ids = torrent_ids
+
+
+class OpenSortOrder(Message):
+    pass
+
+
+class OpenSearch(Message):
+    pass
+
+
+class OpenPreferences(Message):
+    pass
+
+
+class PageChanged(Message):
+    def __init__(self, state: PageState) -> None:
+        super().__init__()
+        self.state = state
