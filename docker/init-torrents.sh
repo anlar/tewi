42c9cd2f96112777a7c1e223f25d61b97dcf3d59
Anton Larionov <11796525+anlar@users.noreply.github.com>
2025-11-12 09:34:01 +0100

    chore: add multiple magnets with random priorities for test env

--- docker/init-torrents.sh
+++ docker/init-torrents.sh
@@ -1,6 +1,14 @@
 #! /usr/bin/env sh
 
-MAGNET='magnet:?xt=urn:btih:d4487f489d4ee786f99bcdeeb8d3f226694ea27f&dn=archlinux-2025.11.01-x86_64.iso'
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-transmission-remote localhost:9092 --add "$MAGNET"
+cat "$SCRIPT_DIR/magnets.txt" | xargs -L 1 transmission-remote localhost:9092 --add
 
+transmission-remote localhost:9092 -l | awk 'NR>1 && !/^Sum:/ {print $1}' | while read id; do 
+  rand=$(od -An -N1 -tu1 /dev/urandom | awk '{print $1 % 3}')
+  case $rand in
+    0) transmission-remote localhost:9092 -t "$id" -Bh ;;
+    1) transmission-remote localhost:9092 -t "$id" -Bl ;;
+    2) transmission-remote localhost:9092 -t "$id" -Bn ;;
+  esac
+done
