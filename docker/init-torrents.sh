dbb3fa587d3c90b1301a6919df199b9c0024c548
Anton Larionov <11796525+anlar@users.noreply.github.com>
2025-12-29 17:02:02 +0100

    chore: move client ports to parameters in init-torrents.sh

--- docker/init-torrents.sh
+++ docker/init-torrents.sh
@@ -4,20 +4,25 @@ LIMIT=${1:-2}
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
+# Port configuration
+TRANSMISSION_PORT=9070
+QBITTORRENT_PORT=9071
+DELUGE_PORT=9072
+
 # Transmission
 
-head -n $LIMIT "$SCRIPT_DIR/magnets.txt" | xargs -L 1 transmission-remote localhost:9070 --add
+head -n $LIMIT "$SCRIPT_DIR/magnets.txt" | xargs -L 1 transmission-remote localhost:$TRANSMISSION_PORT --add
 
-transmission-remote localhost:9070 -l | awk 'NR>1 && !/^Sum:/ {print $1}' | while read id; do
+transmission-remote localhost:$TRANSMISSION_PORT -l | awk 'NR>1 && !/^Sum:/ {print $1}' | while read id; do
   rand=$(od -An -N1 -tu1 /dev/urandom | awk '{print $1 % 3}')
   case $rand in
-    0) transmission-remote localhost:9070 -t "$id" -Bh ;;
-    1) transmission-remote localhost:9070 -t "$id" -Bl ;;
-    2) transmission-remote localhost:9070 -t "$id" -Bn ;;
+    0) transmission-remote localhost:$TRANSMISSION_PORT -t "$id" -Bh ;;
+    1) transmission-remote localhost:$TRANSMISSION_PORT -t "$id" -Bl ;;
+    2) transmission-remote localhost:$TRANSMISSION_PORT -t "$id" -Bn ;;
   esac
 done
 
-transmission-remote localhost:9070 --alt-speed
+transmission-remote localhost:$TRANSMISSION_PORT --alt-speed
 
 # qBittorrent
 
@@ -25,27 +30,27 @@ qbt_password=$(docker logs tewi-qbittorrent-dev 2>&1 | grep 'temporary password'
 
 echo "qbt password: $qbt_password"
 
-qbt_session=$(curl -i --header 'Referer: http://localhost:9071' --data "username=admin&password=$qbt_password" http://localhost:9071/api/v2/auth/login | grep set-cookie | cut -d';' -f1 | cut -d'=' -f2)
+qbt_session=$(curl -i --header "Referer: http://localhost:$QBITTORRENT_PORT" --data "username=admin&password=$qbt_password" http://localhost:$QBITTORRENT_PORT/api/v2/auth/login | grep set-cookie | cut -d';' -f1 | cut -d'=' -f2)
 
 echo "qbt session: $qbt_session"
 
-head -n $LIMIT "$SCRIPT_DIR/magnets.txt" | xargs -I {} curl -X POST -H "Referer: http://localhost:9071" -b "SID=$qbt_session" --data-urlencode "urls={}" http://localhost:9071/api/v2/torrents/add
+head -n $LIMIT "$SCRIPT_DIR/magnets.txt" | xargs -I {} curl -X POST -H "Referer: http://localhost:$QBITTORRENT_PORT" -b "SID=$qbt_session" --data-urlencode "urls={}" http://localhost:$QBITTORRENT_PORT/api/v2/torrents/add
 
-curl -X POST -H "Referer: http://localhost:9071" -b "SID=$qbt_session" http://localhost:9071/api/v2/transfer/toggleSpeedLimitsMode
+curl -X POST -H "Referer: http://localhost:$QBITTORRENT_PORT" -b "SID=$qbt_session" http://localhost:$QBITTORRENT_PORT/api/v2/transfer/toggleSpeedLimitsMode
 
 # Deluge
 
-deluge_session=$(curl -i -X POST http://localhost:9072/json -H "Content-Type: application/json" -d '{"method": "auth.login","params": ["deluge"],"id": 1}' | grep Set-Cookie | cut -d';' -f1 | cut -d'=' -f2)
+deluge_session=$(curl -i -X POST http://localhost:$DELUGE_PORT/json -H "Content-Type: application/json" -d '{"method": "auth.login","params": ["deluge"],"id": 1}' | grep Set-Cookie | cut -d';' -f1 | cut -d'=' -f2)
 
 echo "deluge session: $deluge_session"
 
-deluge_host=$(curl -X POST http://localhost:9072/json -H "Content-Type: application/json" -d '{"method": "web.get_hosts", "params": [], "id": 2}' --cookie "_session_id=$deluge_session" | jq -r '.result[0][0]')
+deluge_host=$(curl -X POST http://localhost:$DELUGE_PORT/json -H "Content-Type: application/json" -d '{"method": "web.get_hosts", "params": [], "id": 2}' --cookie "_session_id=$deluge_session" | jq -r '.result[0][0]')
 
 echo "deluge host: $deluge_host"
 
-curl -X POST http://localhost:9072/json -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$deluge_host'"], "id": 3}' --cookie "_session_id=$deluge_session"
+curl -X POST http://localhost:$DELUGE_PORT/json -H "Content-Type: application/json" -d '{"method": "web.connect", "params": ["'$deluge_host'"], "id": 3}' --cookie "_session_id=$deluge_session"
 
-head -n $LIMIT "$SCRIPT_DIR/magnets.txt" | xargs -I }{ curl -X POST http://localhost:9072/json -H "Content-Type: application/json" -d '{"method": "core.add_torrent_magnet","params": ["}{", {}],"id": 1}' --cookie "_session_id=$deluge_session"
+head -n $LIMIT "$SCRIPT_DIR/magnets.txt" | xargs -I }{ curl -X POST http://localhost:$DELUGE_PORT/json -H "Content-Type: application/json" -d '{"method": "core.add_torrent_magnet","params": ["}{", {}],"id": 1}' --cookie "_session_id=$deluge_session"
 
-curl -X POST http://localhost:9072/json -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"max_download_speed": 100, "max_upload_speed": 100}], "id": 4}' --cookie "_session_id=$deluge_session"
+curl -X POST http://localhost:$DELUGE_PORT/json -H "Content-Type: application/json" -d '{"method": "core.set_config", "params": [{"max_download_speed": 100, "max_upload_speed": 100}], "id": 4}' --cookie "_session_id=$deluge_session"
 
